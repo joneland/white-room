df10c76dd5d1b72c3797f89aeb410e9b28c956f6
joneland <jonathan.eland@gmail.com>
2016-06-29 12:41:04 +0100

    Adding ring and compojure dependencies to the project

---
+++ white-room/project.clj
@@ -0,0 +1,5 @@
+(defproject white-room "0.1-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-core "1.5.0"]
+                 [ring/ring-jetty-adapter "1.5.0"]
+                 [compojure "1.5.1"]])
