500c332eead4b713ab82ca51152c0befba6b02d8
joneland <jonathan.eland@gmail.com>
2016-06-30 19:44:44 +0100

    Webapp now runs in a docker container

--- white-room/project.clj
+++ white-room/project.clj
@@ -1,9 +1,13 @@
 (defproject white-room "0.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [http-kit "2.1.19"]
                  [ring/ring-core "1.5.0"]
                  [ring/ring-jetty-adapter "1.5.0"]
                  [ring/ring-json "0.4.0"]
                  [compojure "1.5.1"]]
+
+  :main white-room.handler
+  :aot [white-room.handler]
 
   :plugins [[lein-ring "0.9.7"]]  
 
--- white-room/src/white_room/handler.clj
+++ white-room/src/white_room/handler.clj
@@ -1,8 +1,10 @@
 (ns white-room.handler
-  (:require [compojure.core :refer :all]
+  (:require [org.httpkit.server :refer [run-server]]
+            [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.json :refer :all]
-            [ring.util.response :refer [response]]))
+            [ring.util.response :refer [response]])
+  (:gen-class))
 
 (defroutes app-routes
   (route/not-found
@@ -12,3 +14,8 @@
  (-> app-routes
      wrap-json-response
      wrap-json-body))
+
+(defn -main []
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))]
+    (run-server app {:port port})
+    (println (str "Listening on port " port))))
